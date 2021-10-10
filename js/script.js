3405eaa044822bc00d200516a720491eb3f42f02
Aditya Shah <aditya7395@gmail.com>
2021-10-10 10:53:59 -0700

    Completed the form layout and reset all fields on clicking the cancel button

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function register(){ 
+    alert("Thank you for registering!");
+}
+
+function resetForm(){ 
+	document.getElementById('user-id').value="";
+	document.getElementById('password').value="";
+	document.getElementById('verify-password').value="";
+	document.getElementById('email').value="";
+	document.getElementById('verify-email').value="";
+    document.getElementById('security-q1').value="--";
+	document.getElementById('security-q2').value="--";
+	document.getElementById('security-a1').value="";
+	document.getElementById('security-a1').value="";
+	document.getElementById('phone').value="";
+	document.getElementById('address').value="";
+	document.getElementById('areas').value="";
+}
