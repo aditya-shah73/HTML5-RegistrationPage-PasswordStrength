532e80c906da7bc7135450d85ac5e4159630cd47
Aditya Shah <aditya7395@gmail.com>
2021-10-10 11:57:52 -0700

    Added password requirement block logic

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
-function register(){ 
-    alert("Thank you for registering!");
+function showPasswordRequirements() {
+  document.getElementById("password-requirement-text").style.display = "block";
+  document.getElementById("invalid-password").style.display = "none";
 }
 
-function resetForm(){ 
-	document.getElementById('user-id').value="";
-	document.getElementById('password').value="";
-	document.getElementById('verify-password').value="";
-	document.getElementById('email').value="";
-	document.getElementById('verify-email').value="";
-    document.getElementById('security-q1').value="--";
-	document.getElementById('security-q2').value="--";
-	document.getElementById('security-a1').value="";
-	document.getElementById('security-a1').value="";
-	document.getElementById('phone').value="";
-	document.getElementById('address').value="";
-	document.getElementById('areas').value="";
+function hidePasswordRequirements() {
+  document.getElementById("password-requirement-text").style.display = "none";
+  document.getElementById("invalid-password").style.display = "block";
+}
+
+function verifyPassword() {
+    document.getElementById("password-requirement-text").style.display = "none";
+    document.getElementById("invalid-password").style.display = "none";
+}
+
+function register() {
+  alert("Thank you for registering!");
+}
+
+function resetForm() {
+  document.getElementById("user-id").value = "";
+  document.getElementById("password").value = "";
+  document.getElementById("verify-password").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("verify-email").value = "";
+  document.getElementById("security-q1").value = "--";
+  document.getElementById("security-q2").value = "--";
+  document.getElementById("security-a1").value = "";
+  document.getElementById("security-a1").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("areas").value = "";
 }
